64a43e62d0cd18a5682fd415b6d6f046c1a5b1ee
André Silva <2493377+askpt@users.noreply.github.com>
2021-12-29 19:05:08 +0000

    Completed Manage execution flow in F# from mslearn

---
+++ fsharp-conditional/Cards/Program.fs
@@ -0,0 +1,15 @@
+﻿let cardNo = 12
+
+let cardDescription =
+    if cardNo = 1 || cardNo = 14 then
+        "Ace"
+    else if cardNo = 11 then
+        "Jack"
+    else if cardNo = 12 then
+        "Queen"
+    else if cardNo = 13 then
+        "King"
+    else
+        string cardNo
+
+printfn "The card is %s" cardDescription
---
+++ fsharp-conditional/Loops/Program.fs
@@ -0,0 +1,21 @@
+﻿let cardDescription (card: int) : string =
+    let cardNo: int = card % 13
+
+    if cardNo = 1 then "Ace"
+    else if cardNo = 11 then "Jack"
+    else if cardNo = 12 then "Queen"
+    else if cardNo = 0 then "King"
+    else string cardNo
+
+let suit (no: int) : string =
+    let suitNo: int = no / 13
+
+    if suitNo = 0 then "Hearts"
+    else if suitNo = 1 then "Spades"
+    else if suitNo = 2 then "Diamonds"
+    else "Clubs"
+
+let cards = [ 1; 10; 2; 34 ]
+
+for card in cards do
+    printfn "%s of %s" (cardDescription (card)) (suit (card))
