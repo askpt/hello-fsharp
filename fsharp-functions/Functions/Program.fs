ba0ea73ee7e9a896ccffb13f0071b81dd6758f55
André Silva <2493377+askpt@users.noreply.github.com>
2021-12-30 12:14:50 +0000

    Completed Create and architect with functions in F# from mslearn

---
+++ fsharp-functions/Functions/Program.fs
@@ -0,0 +1,10 @@
+﻿let cardFace (card: int) : string =
+    let no = card % 13
+
+    if no = 1 then "Ace"
+    else if no = 11 then "Jack"
+    else if no = 12 then "Queen"
+    else if no = 0 then "King"
+    else string no
+
+printfn "%s" (cardFace 11)
