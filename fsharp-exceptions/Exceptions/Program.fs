2e43519932dc8a84f494c7e7c7960c2f55f2c823
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-05 11:26:27 +0000

    Play with exceptions

---
+++ fsharp-exceptions/Exceptions/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let exceptionNull (value) : string =
+    ArgumentNullException.ThrowIfNull value
+    string value
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+try
+    let value = exceptionNull null
+    printfn "%s" value
+with
+| Failure e -> printfn "%s" e
