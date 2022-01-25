36ab5e50f60b814535d13514203270cb390d6567
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-25 09:48:39 +0000

    Completed the Lucian's Luscious Lasagna

---
+++ fsharp-exercism/LuciansLusciousLasagna/Program.fsx
@@ -0,0 +1,13 @@
+﻿let expectedMinutesInOven: int = 40
+
+let remainingMinutesInOven (actualMinutes: int) : int = expectedMinutesInOven - actualMinutes
+let preparationTimeInMinutes (layers: int) : int = layers * 2
+
+let elapsedTimeInMinues (layers: int, minutesInOven: int) : int =
+    preparationTimeInMinutes (layers)
+    + remainingMinutesInOven (minutesInOven)
+
+
+printfn $"Remaining minutes in oven: ${remainingMinutesInOven (30)}"
+printfn $"Layers: ${preparationTimeInMinutes (2)}"
+printfn $"Elapsed time: ${elapsedTimeInMinues (3, 20)}"
